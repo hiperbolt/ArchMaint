ea85f310f83e0c1901bb99a38ce332ee75c77389
Tomás Simões <tomasimoes03@gmail.com>
2016-06-14 16:06:25 +0100

    Create archmaint.py

---
+++ archmaint.py
@@ -0,0 +1,93 @@
+from tkinter import *
+from tkinter import messagebox
+from sys import exit
+from subprocess import *
+import os
+
+__author__ = 'hiperbolt'
+
+class RootWindow:
+    def __init__(self, window):
+        self.window = window
+        self.window.geometry('1000x800')
+        self.window.title('ArchMaint')
+        self.frame1 = Frame(self.window)
+        self.frame2 = Frame(self.window)
+        self.label1 = Label(self.frame1, text='Welcome to ArchMaint', font=5)
+        self.button1 = Button(self.frame2, text='Update Pacman Packages', width='20',
+                              command=lambda: self.updatepackges())
+        self.button2 = Button(self.frame2, text='Clear Pacman Cache', width='20', command=lambda: self.clearcache())
+        self.button3 = Button(self.frame2, text='Clear Orphaned Packages', width='20',
+                              command=lambda: self.clearorphaned())
+        self.button4 = Button(self.frame2, text='Optimize Package Database', width='20',
+                              command=lambda: self.optimizedatabase())
+        self.button5 = Button(self.frame2, text='Clear Broken Symlinks', width='20',
+                              command=lambda: self.clearsymlinks())
+        self.button6 = Button(self.frame2, text='Clear TempFiles', width='20', command=lambda: self.cleartemps())
+        self.button7 = Button(self.frame2, text='Credits', width='20', command=lambda: messagebox.showinfo("Credits",
+                                                                                                           "Originally Made by: hiperbolt / Tomás Simões (tomasimoes03@gmail.com)"))
+        self.button8 = Button(self.frame2, text='Exit', width='20', command=lambda: exit())
+        self.frame1.pack(anchor='nw')
+        self.frame2.pack(anchor='center')
+        self.label1.pack()
+        self.button1.pack()
+        self.button2.pack()
+        self.button3.pack()
+        self.button4.pack()
+        self.button5.pack()
+        self.button6.pack()
+        self.button7.pack()
+        self.button8.pack()
+
+    def updatepackges(self):
+        try:
+            check_call('pacman --noconfirm -Syyu', shell=True)
+        except CalledProcessError:
+            messagebox.showerror('Error', 'An error occurred, that is all we know.')
+        else:
+            messagebox.showinfo("Sucess", "Pacman Packages Updated!")
+
+    def clearcache(self):
+        try:
+            check_call("pacman --noconfirm -Sc", shell=True)
+        except CalledProcessError:
+            messagebox.showerror('Error', 'An error occurred, that is all we know.')
+        else:
+            messagebox.showinfo('Success', 'Pacman Package Cache Cleared!')
+
+    def clearorphaned(self):
+        try:
+            check_call('pacman -Rns $(pacman -Qtdq)', shell=True)
+        except CalledProcessError:
+            messagebox.showerror('Error', 'Either you had no orphaned packages or something wrong occurred.')
+        else:
+            messagebox.showinfo('Success', 'Orphaned Packages Cleared')
+
+    def optimizedatabase(self):
+        try:
+            check_call('pacman-optimize --nocolor', shell=True)
+        except CalledProcessError:
+            messagebox.showerror('Error', 'An error occurred, that is all we know.')
+        else:
+            messagebox.showinfo('Success', 'Pacman Database Optimized.')
+
+    def clearsymlinks(self):
+        try:
+            check_call('find . -type l -! -exec test -e {} \; -delete', shell=True)
+        except CalledProcessError:
+            messagebox.showerror('Error', 'An error occurred, that is all we know.')
+        else:
+            messagebox.showinfo('Success', 'Broken Symlinks Cleared.')
+
+    def cleartemps(self):
+        try:
+            check_call('python rmshit.py', shell=True)
+        except CalledProcessError:
+            messagebox.showerror('Error', 'An error occurred, that is all we know.')
+        else:
+            messagebox.showinfo('Success', 'TempFiles deleted, thanks rmshit.py!')
+
+if __name__ == '__main__':
+    root = Tk()
+    RootWindow(root)
+    root.mainloop()
